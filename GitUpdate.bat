d7f0fb9761be0841376ab9a59f39f505d5d17f0b
Ibrahim Ali <81215408+DARK8965@users.noreply.github.com>
2024-08-27 01:34:33 +0300

    New Update [Remove Credit] 28/5/2024 || Tue 08/27/2024 |  1:34:33.38

--- GitUpdate.bat
+++ GitUpdate.bat
@@ -1,10 +1,5 @@
-timeout 8
-cls
-git pull https://github.com/IbrahimAli768/Maker-free.git -f
-cls
-timeout 8
-cls
+git pull https://github.com/DARK8965/Bot_Maker_Free.git -f
 echo [ Done Update All Files ]
-timeout 8
+
 exit
 
